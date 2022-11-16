0f709787bf3c8120991b7b413531ca5e563d0219
2udy <22udy629@gmail.com>
2022-11-16 16:34:37 +0900

    get data fixtures

--- final-pjt-back/movies/urls.py
+++ final-pjt-back/movies/urls.py
@@ -3,5 +3,6 @@ from . import views
 
 app_name = 'movies'
 urlpatterns = [
-    path('',views.movie_list)
+    path('',views.movie_list),
+    path('save/',views.get_movie)
 ]
--- final-pjt-back/movies/views.py
+++ final-pjt-back/movies/views.py
@@ -1,6 +1,9 @@
 from rest_framework.response import Response
 from rest_framework.decorators import api_view
 from django.shortcuts import render
+import requests
+
+import json
 
 from .models import Movie
 from .serializers import MovieListSerializer
@@ -12,3 +15,22 @@ def movie_list(request):
     movies = Movie.objects.all()
     serializer = MovieListSerializer(movies, many=True)
     return Response(serializer.data)
+    
+
+def get_movie(request):
+    response = requests.get("https://api.themoviedb.org/3/movie/top_rated?api_key=f5c70cf3de1ffb0fae4f5469051c4be3&language=ko-kr").json()
+    results = response.get('results')
+    
+    for result in results:
+        movie = Movie()
+        movie.id = result.get('id')
+        movie.poster_path = result.get('poster_path')
+        movie.popularity = result.get('popularity')
+        movie.genre_ids = result.get('genre_ids')
+        movie.vote_average = result.get('vote_average')
+        movie.genre_ids = result.get('genre_ids')
+        movie.overview = result.get('overview')
+        movie.release_date = result.get('release_date')
+        movie.title = result.get('title')
+    
+    open("mydata-new.json","wb").write(open("mydata.json").read().decode("unicode_escape").encode("utf8"))
