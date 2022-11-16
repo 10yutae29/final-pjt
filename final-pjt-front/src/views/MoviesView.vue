811552cf5bb768be3981b12dc69b7d721bdf2631
10yutae29 <10yutae29@gmail.com>
2022-11-16 16:37:27 +0900

    11/16-front-morning

---
+++ final-pjt-front/src/views/MoviesView.vue
@@ -0,0 +1,20 @@
+<template>
+  <div>
+    <MoviesList/>
+  </div>
+</template>
+
+<script>
+import MoviesList from '@/components/MoviesList'
+
+export default {
+  name: 'MoviesView',
+  components: {
+    MoviesList
+  }
+}
+</script>
+
+<style>
+
+</style>
