4866cee16ff3d75da5042c38868937075e042fa8
2udy <22udy629@gmail.com>
2022-11-17 21:09:24 +0900

    movie comment

--- final-pjt-back/community/serializers.py
+++ final-pjt-back/community/serializers.py
@@ -8,6 +8,7 @@ class CommentListSerializer(serializers.ModelSerializer):
         fields = '__all__'
 
 
+# 댓글 작성과 수정에만 사용하는 시리얼라이저로, content만 들어가 져도 됨,,,, 그 유저랑 무비는 자동으로 주어질텐데....ㅇㅂㅇ?
 class CommentSerializer(serializers.ModelSerializer):
 
     class Meta:
--- final-pjt-back/movies/serializers.py
+++ final-pjt-back/movies/serializers.py
@@ -1,5 +1,6 @@
 from rest_framework import serializers
 from .models import Movie
+from community.models import Comment
 
 class MovieListSerializer(serializers.ModelSerializer):
 
@@ -8,7 +9,15 @@ class MovieListSerializer(serializers.ModelSerializer):
         fields = '__all__'
 
 
+class CommentSerializer(serializers.ModelSerializer):
+
+    class Meta:
+        model = Comment
+        fields = '__all__'
+
+
 class MovieSerializer(serializers.ModelSerializer):
+    comment_set = CommentSerializer(many=True, read_only=True)
     
     class Meta:
         model = Movie
