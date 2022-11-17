dfbc8b0b0299a59f23004d0539bbd56fe128857d
2udy <22udy629@gmail.com>
2022-11-17 20:54:09 +0900

    comment CRUD

--- final-pjt-back/community/urls.py
+++ final-pjt-back/community/urls.py
@@ -3,6 +3,7 @@ from . import views
 
 app_name = 'community'
 urlpatterns = [
-    path('comment/',views.comment),
+    path('comment/',views.comment_list),
+    path('comment/<int:comment_pk>/',views.comment_detail),
     path('like/',views.like),
 ]
