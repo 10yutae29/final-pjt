aa985322740269b009781ff8ba3d9d5b3a37a447
2udy <22udy629@gmail.com>
2022-11-17 10:59:22 +0900

    auth

--- final-pjt-back/finalpjt/settings.py
+++ final-pjt-back/finalpjt/settings.py
@@ -78,7 +78,7 @@ SITE_ID = 1
 
 REST_FRAMEWORK = {
     'DEFAULT_PERMISSION_CLASSES': [
-        'rest_framework.permissions.IsAuthenticated',
+        'rest_framework.permissions.AllowAny',
     ]
 }
 
