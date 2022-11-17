f9df9e5727af49e0cdba60499c881576b9e997bd
10yutae29 <10yutae29@gmail.com>
2022-11-17 14:18:32 +0900

    afterdetail

---
+++ final-pjt-front/src/components/CommentsListItems.vue
@@ -0,0 +1,16 @@
+<template>
+  <div>
+    <h1>CommentsListItem</h1>
+    <p>댓글 하나하나</p>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'CommentsListItem'
+}
+</script>
+
+<style>
+
+</style>
--- final-pjt-front/src/components/MoviesListItem.vue
+++ final-pjt-front/src/components/MoviesListItem.vue
@@ -1,5 +1,5 @@
 <template>
-  <div id="a" class="col-xxl-3 col-md-6 mb-3">
+  <div id="a" class="col-xxl-3 col-md-6 mb-3" @click="goDetail">
     <div class="row justify-content-center">
       <div class="w-75 border col-12 card" id="card">
         <img class="mt-3" :src="`https://image.tmdb.org/t/p/original${movie?.poster_path}`" alt="">
@@ -39,6 +39,9 @@ export default {
     },
     cancelLike() {
       this.isLiked = false
+    },
+    goDetail() {
+      this.$router.push(`/moviesview/${this.movie.id}`)
     }
   }
 }
