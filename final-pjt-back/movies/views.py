1a82c26961f7eb9a0e36189053ab38689ef07570
2udy <22udy629@gmail.com>
2022-11-22 16:47:33 +0900

    genre in ko-kr

--- final-pjt-back/movies/views.py
+++ final-pjt-back/movies/views.py
@@ -6,6 +6,7 @@ import requests
 from rest_framework import status
 from .models import Movie, Genre, Prefer
 from .serializers import MovieListSerializer, MovieSerializer, GenreSerializer
+from pprint import pprint
 
 # Create your views here.
 
@@ -66,29 +67,31 @@ def movie_likes(request, movie_pk):
 
 
 def get_movie(request):
-    response = requests.get("https://api.themoviedb.org/3/movie/top_rated?api_key=f5c70cf3de1ffb0fae4f5469051c4be3&language=ko-kr").json()
-    results = response.get('results')
-    
-    print(results)
-    for result in results:
-        movie = Movie()
-        movie.id = result.get('id')
-        movie.poster_path = result.get('poster_path')
-        movie.popularity = result.get('popularity')
-        movie.vote_average = result.get('vote_average')
-        movie.overview = result.get('overview')
-        movie.release_date = result.get('release_date')
-        movie.title = result.get('title')
-        movie.save()
+    for i in range(10):
+        results = requests.get(f"https://api.themoviedb.org/3/list/{i}?api_key=f5c70cf3de1ffb0fae4f5469051c4be3&language=ko-kr").json()
+        
+        pprint(results)
+        print(type(results))
+        for result in results:
+            pprint(result)
+            print(type(result))        #     movie = Movie()
+        #     movie.id = result.get('id')
+        #     movie.poster_path = result.get('poster_path')
+        #     movie.popularity = result.get('popularity')
+        #     movie.vote_average = result.get('vote_average')
+        #     movie.overview = result.get('overview')
+        #     movie.release_date = result.get('release_date')
+        #     movie.title = result.get('title')
+        #     movie.save()
 
-        genre_ids = result.get('genre_ids')
-        for genre_id in genre_ids:
-            genre = Genre.objects.get(pk=genre_id)
-            movie.genres.add(genre)
+        # genre_ids = result.get('genre_ids')
+        # for genre_id in genre_ids:
+        #     genre = Genre.objects.get(pk=genre_id)
+        #     movie.genres.add(genre)
             
 
 def get_genres(request):
-    results = requests.get("https://api.themoviedb.org/3/genre/movie/list?api_key=f5c70cf3de1ffb0fae4f5469051c4be3").json().get('genres')
+    results = requests.get("https://api.themoviedb.org/3/genre/movie/list?api_key=f5c70cf3de1ffb0fae4f5469051c4be3&language=ko-kr").json().get('genres')
     
     for result in results:
         genre = Genre()
