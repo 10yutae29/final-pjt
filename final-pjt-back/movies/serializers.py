48f0c7f1909f6991fefd8991467dab1128105235
2udy <22udy629@gmail.com>
2022-11-22 09:08:40 +0900

    comment user info

--- final-pjt-back/movies/serializers.py
+++ final-pjt-back/movies/serializers.py
@@ -12,16 +12,26 @@ class MovieListSerializer(serializers.ModelSerializer):
 
 
 
-class CommentSerializer(serializers.ModelSerializer):
-    
-    class Meta:
-        model = Comment
-        fields = '__all__'
 
 
 class MovieSerializer(serializers.ModelSerializer):
+
+    class CommentSerializer(serializers.ModelSerializer):
+
+        class UserSerializer(serializers.ModelSerializer):
+
+            class Meta:
+                model = get_user_model()
+                fields = ('id', 'username' )
+        
+        user = UserSerializer(read_only=True)
+        
+        class Meta:
+            model = Comment
+            fields = '__all__'
+
     comment_set = CommentSerializer(many=True, read_only=True)
-    
+
     class Meta:
         model = Movie
         fields = '__all__'
