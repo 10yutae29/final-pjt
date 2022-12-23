fb2a464c393e7a22b213b1110fa4dfdcc5e45c33
10yutae29 <10yutae29@gmail.com>
2022-12-23 12:18:03 +0900

    user_info

--- final-pjt-back/accounts/urls.py
+++ final-pjt-back/accounts/urls.py
@@ -1,8 +1,10 @@
-from django.urls import path
+from django.urls import path, include
 from . import views
+
 
 app_name = 'accounts'
 urlpatterns = [
     path('<int:user_pk>/',views.user_detail),
     path('recommend/<int:user_pk>/',views.recommend, name='recommend'),
+    path('<int:user_pk>/userchange/', views.userchange,),
 ]
